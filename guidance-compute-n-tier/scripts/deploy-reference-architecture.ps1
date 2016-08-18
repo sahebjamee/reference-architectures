5d335ff81a55ec3e34012066537dd7a6b71892ab
Kirpa Singh <kirpas@microsoft.com>
2016-08-18 15:16:07 -0700

    Empty inbound NAT rules are required

--- guidance-compute-n-tier/scripts/deploy-reference-architecture.ps1
+++ guidance-compute-n-tier/scripts/deploy-reference-architecture.ps1
@@ -11,27 +11,38 @@ param(
   $OSType = "Linux"
 )
 
-$resourceGroupName = 'app1-dev-rg'
+$ErrorActionPreference = "Stop"
 
-$templateRootUri = New-Object System.Uri -ArgumentList @('https://raw.githubusercontent.com/mspnp/arm-building-blocks/kirpas/vm-name-fix/ARMBuildingBlocks/Templates/')
+$templateRootUriString = $env:TEMPLATE_ROOT_URI
+if ($templateRootUriString -eq $null) {
+  $templateRootUriString = "https://raw.githubusercontent.com/mspnp/template-building-blocks/master/"
+}
+
+if (![System.Uri]::IsWellFormedUriString($templateRootUriString, [System.UriKind]::Absolute)) {
+  throw "Invalid value for TEMPLATE_ROOT_URI: $env:TEMPLATE_ROOT_URI"
+}
+
+Write-Host
+Write-Host "Using $templateRootUriString to locate templates"
+Write-Host
 
 # Deployer templates for respective resources
-$virtualNetworkTemplate = New-Object System.Uri -ArgumentList @($templateRootUri, 'buildingBlocks/vnet-n-subnet/azuredeploy.json')
-$loadBalancedVmSetTemplate = New-Object System.Uri -ArgumentList @($templateRootUri, 'buildingBlocks/loadBalancer-backend-n-vm/azuredeploy.json')
-$virtualMachineTemplate = New-Object System.Uri -ArgumentList @($templateRootUri, 'buildingBlocks/multi-vm-n-nic-m-storage/azuredeploy.json')
-$networkSecurityGroupTemplate = New-Object System.Uri -ArgumentList @($templateRootUri, 'resources/Microsoft.Network/networkSecurityGroups/networkSecurityGroups.json')
+$templateRootUri = New-Object System.Uri -ArgumentList @($templateRootUriString)
+$virtualNetworkTemplate = New-Object System.Uri -ArgumentList @($templateRootUri, 'templates/buildingBlocks/vnet-n-subnet/azuredeploy.json')
+$loadBalancedVmSetTemplate = New-Object System.Uri -ArgumentList @($templateRootUri, 'templates/buildingBlocks/loadBalancer-backend-n-vm/azuredeploy.json')
+$virtualMachineTemplate = New-Object System.Uri -ArgumentList @($templateRootUri, 'templates/buildingBlocks/multi-vm-n-nic-m-storage/azuredeploy.json')
+$networkSecurityGroupTemplate = New-Object System.Uri -ArgumentList @($templateRootUri, 'templates/buildingBlocks/networkSecurityGroups/azuredeploy.json')
 
 # Template parameters for respective deployments
-$virtualNetworkParametersFile = [System.IO.Path]::Combine($PSScriptRoot, '..\Parameters', $OSType.ToLower(), 'virtualNetwork.parameters.json')
-$businessTierParametersFile = [System.IO.Path]::Combine($PSScriptRoot, '..\Parameters', $OSType.ToLower(), 'businessTier.parameters.json')
-$dataTierParametersFile = [System.IO.Path]::Combine($PSScriptRoot, '..\Parameters', $OSType.ToLower(), 'dataTier.parameters.json')
-$webTierParametersFile = [System.IO.Path]::Combine($PSScriptRoot, '..\Parameters', $OSType.ToLower(), 'webTier.parameters.json')
-$managementTierParametersFile = [System.IO.Path]::Combine($PSScriptRoot, '..\Parameters', $OSType.ToLower(), 'managementTier.parameters.json')
-$networkSecurityGroupParametersFile = [System.IO.Path]::Combine($PSScriptRoot, '..\Parameters', $OSType.ToLower(), 'networkSecurityGroup.parameters.json')
+$virtualNetworkParametersFile = [System.IO.Path]::Combine($PSScriptRoot, '..\parameters', $OSType.ToLower(), 'virtualNetwork.parameters.json')
+$businessTierParametersFile = [System.IO.Path]::Combine($PSScriptRoot, '..\parameters', $OSType.ToLower(), 'businessTier.parameters.json')
+$dataTierParametersFile = [System.IO.Path]::Combine($PSScriptRoot, '..\parameters', $OSType.ToLower(), 'dataTier.parameters.json')
+$webTierParametersFile = [System.IO.Path]::Combine($PSScriptRoot, '..\parameters', $OSType.ToLower(), 'webTier.parameters.json')
+$managementTierParametersFile = [System.IO.Path]::Combine($PSScriptRoot, '..\parameters', $OSType.ToLower(), 'managementTier.parameters.json')
+$networkSecurityGroupParametersFile = [System.IO.Path]::Combine($PSScriptRoot, '..\parameters', $OSType.ToLower(), 'networkSecurityGroups.parameters.json')
 
 # Login to Azure and select your subscription
-Login-AzureRmAccount | Out-Null
-Select-AzureRmSubscription -SubscriptionId $SubscriptionId | Out-Null
+Login-AzureRmAccount -SubscriptionId $SubscriptionId | Out-Null
 
 # Create the resource group
 $resourceGroup = New-AzureRmResourceGroup -Name $resourceGroupName -Location $Location
@@ -45,17 +56,17 @@ New-AzureRmResourceGroupDeployment -Name "ra-ntier-biz-deployment" -ResourceGrou
     -TemplateUri $loadBalancedVmSetTemplate.AbsoluteUri -TemplateParameterFile $businessTierParametersFile
 
 Write-Host "Deploying data tier..."
-New-AzureRmResourceGroupDeployment -Name "ra-ntier-biz-deployment" -ResourceGroupName $resourceGroup.ResourceGroupName `
+New-AzureRmResourceGroupDeployment -Name "ra-ntier-data-deployment" -ResourceGroupName $resourceGroup.ResourceGroupName `
     -TemplateUri $loadBalancedVmSetTemplate.AbsoluteUri -TemplateParameterFile $dataTierParametersFile
 
 Write-Host "Deploying web tier..."
-New-AzureRmResourceGroupDeployment -Name "ra-ntier-biz-deployment" -ResourceGroupName $resourceGroup.ResourceGroupName `
+New-AzureRmResourceGroupDeployment -Name "ra-ntier-web-deployment" -ResourceGroupName $resourceGroup.ResourceGroupName `
     -TemplateUri $loadBalancedVmSetTemplate.AbsoluteUri -TemplateParameterFile $webTierParametersFile
 
 Write-Host "Deploying management tier..."
-New-AzureRmResourceGroupDeployment -Name "ra-ntier-biz-deployment" -ResourceGroupName $resourceGroup.ResourceGroupName `
+New-AzureRmResourceGroupDeployment -Name "ra-ntier-mgmt-deployment" -ResourceGroupName $resourceGroup.ResourceGroupName `
     -TemplateUri $virtualMachineTemplate.AbsoluteUri -TemplateParameterFile $managementTierParametersFile
 
 Write-Host "Deploying network security group"
-New-AzureRmResourceGroupDeployment -Name "ra-ntier-vm-nsg-deployment" -ResourceGroupName $resourceGroup.ResourceGroupName `
+New-AzureRmResourceGroupDeployment -Name "ra-ntier-nsg-deployment" -ResourceGroupName $resourceGroup.ResourceGroupName `
     -TemplateUri $networkSecurityGroupTemplate.AbsoluteUri -TemplateParameterFile $networkSecurityGroupParametersFile
