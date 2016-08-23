0538309d23329ca131364913f290cb1a2d889130
Andrew Oakley <anoakley@microsoft.com>
2016-08-23 14:55:08 -0700

    Add RRAS extension

---
+++ guidance-iaas-ad/extensions/rras.ps1
@@ -0,0 +1,6 @@
+﻿Install-WindowsFeature Routing  -IncludeAllSubFeature -IncludeManagementTools
+Install-WindowsFeature RSAT-RemoteAccess-PowerShell -IncludeAllSubFeature -IncludeManagementTools
+Install-RemoteAccess -VpnType VpnS2S
+Stop-Service RemoteAccess
+netsh ras set type ipv4rtrtype=LANANDDD ipv6rtrtype=none rastype=none
+Start-Service RemoteAccess 
